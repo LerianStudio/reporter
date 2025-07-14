09bcab6ecee92621179529f84edbb579846c6ba3
arthurkz <arthur.ribeiro@lerian.studio>
2025-07-14 17:53:54 -0300

    fix: Adjusting the code according the lint.

--- pkg/datasource-config.go
+++ pkg/datasource-config.go
@@ -89,6 +89,7 @@ func ConnectToDataSource(databaseName string, dataSource *DataSource, logger log
 // ExternalDatasourceConnections initializes and returns a map of external data source connections.
 func ExternalDatasourceConnections(logger log.Logger) map[string]DataSource {
 	externalDataSources := make(map[string]DataSource)
+
 	dataSourceConfigs := getDataSourceConfigs(logger)
 	for _, dataSource := range dataSourceConfigs {
 		switch strings.ToLower(dataSource.Type) {
@@ -102,8 +103,10 @@ func ExternalDatasourceConnections(logger log.Logger) map[string]DataSource {
 			logger.Errorf("Unsupported database type '%s' for data source '%s'.", dataSource.Type, dataSource.Name)
 			continue
 		}
+
 		logger.Infof("Configured external data source: %s with config name: %s", dataSource.Name, dataSource.ConfigName)
 	}
+
 	return externalDataSources
 }
 
@@ -113,13 +116,16 @@ func initMongoDataSource(dataSource DataSourceConfig, logger log.Logger) DataSou
 	if dataSource.Options != "" {
 		mongoURI += "?" + dataSource.Options
 	}
+
 	var params []string
 	if dataSource.SSL == "true" {
 		params = append(params, "ssl=true")
 	}
+
 	if dataSource.SSLCA != "" {
 		params = append(params, "tlsCAFile="+url.QueryEscape(dataSource.SSLCA))
 	}
+
 	if len(params) > 0 {
 		if strings.Contains(mongoURI, "?") {
 			mongoURI += "&" + strings.Join(params, "&")
@@ -127,8 +133,10 @@ func initMongoDataSource(dataSource DataSourceConfig, logger log.Logger) DataSou
 			mongoURI += "?" + strings.Join(params, "&")
 		}
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		logger.Errorf("Failed to connect to MongoDB [%s]: %v", dataSource.ConfigName, err)
@@ -137,7 +145,9 @@ func initMongoDataSource(dataSource DataSourceConfig, logger log.Logger) DataSou
 	} else {
 		logger.Infof("Successfully connected to MongoDB [%s]", dataSource.ConfigName)
 	}
+
 	_ = client.Disconnect(ctx)
+
 	return DataSource{
 		DatabaseType: MongoDBType,
 		MongoURI:     mongoURI,
@@ -153,6 +163,7 @@ func initPostgresDataSource(dataSource DataSourceConfig, logger log.Logger) Data
 		connectionString += fmt.Sprintf("&sslrootcert=%s", url.QueryEscape(dataSource.SSLRootCert))
 		logger.Infof("Connection string: '%s' ", connectionString)
 	}
+
 	connection := &pg.Connection{
 		ConnectionString:   connectionString,
 		DBName:             dataSource.Database,
@@ -163,6 +174,7 @@ func initPostgresDataSource(dataSource DataSourceConfig, logger log.Logger) Data
 	if err := connection.Connect(); err != nil {
 		logger.Errorf("Failed to connect to Postgres [%s]: %v", dataSource.ConfigName, err)
 	}
+
 	return DataSource{
 		DatabaseType:   dataSource.Type,
 		DatabaseConfig: connection,
