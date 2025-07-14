21600ea94653e24a0c99ccc83de13805f1a3071a
arthurkz <arthur.ribeiro@lerian.studio>
2025-07-14 17:49:14 -0300

    fix: Adjusting the code according the lint.

--- pkg/datasource-config.go
+++ pkg/datasource-config.go
@@ -89,87 +89,85 @@ func ConnectToDataSource(databaseName string, dataSource *DataSource, logger log
 // ExternalDatasourceConnections initializes and returns a map of external data source connections.
 func ExternalDatasourceConnections(logger log.Logger) map[string]DataSource {
 	externalDataSources := make(map[string]DataSource)
-
 	dataSourceConfigs := getDataSourceConfigs(logger)
-
 	for _, dataSource := range dataSourceConfigs {
 		switch strings.ToLower(dataSource.Type) {
 		case MongoDBType:
-			mongoURI := fmt.Sprintf("%s://%s:%s@%s:%s/%s",
-				dataSource.Type, dataSource.User, dataSource.Password, dataSource.Host, dataSource.Port, dataSource.Database)
-
-			if dataSource.Options != "" {
-				mongoURI += "?" + dataSource.Options
-			}
-
-			var params []string
-			if dataSource.SSL == "true" {
-				params = append(params, "ssl=true")
-			}
-			if dataSource.SSLCA != "" {
-				params = append(params, "tlsCAFile="+url.QueryEscape(dataSource.SSLCA))
-			}
-			if len(params) > 0 {
-				if strings.Contains(mongoURI, "?") {
-					mongoURI += "&" + strings.Join(params, "&")
-				} else {
-					mongoURI += "?" + strings.Join(params, "&")
-				}
-			}
-
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
-			client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-			if err != nil {
-				logger.Errorf("Failed to connect to MongoDB [%s]: %v", dataSource.ConfigName, err)
-			} else if err := client.Ping(ctx, nil); err != nil {
-				logger.Errorf("Failed to ping MongoDB [%s]: %v", dataSource.ConfigName, err)
-			} else {
-				logger.Infof("Successfully connected to MongoDB [%s]", dataSource.ConfigName)
-			}
-			_ = client.Disconnect(ctx)
-
-			externalDataSources[dataSource.ConfigName] = DataSource{
-				DatabaseType: MongoDBType,
-				MongoURI:     mongoURI,
-				MongoDBName:  dataSource.Database,
-				Initialized:  false,
-			}
+			ds := initMongoDataSource(dataSource, logger)
+			externalDataSources[dataSource.ConfigName] = ds
 		case PostgreSQLType:
-			connectionString := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=%s",
-				dataSource.Type, dataSource.User, url.QueryEscape(dataSource.Password), dataSource.Host, dataSource.Port, dataSource.Database, dataSource.SSLMode)
-			if dataSource.SSLMode != "" {
-				connectionString += fmt.Sprintf("&sslrootcert=%s", url.QueryEscape(dataSource.SSLRootCert))
-				logger.Infof("Connection string: '%s' ", connectionString)
-			}
-
-			connection := &pg.Connection{
-				ConnectionString:   connectionString,
-				DBName:             dataSource.Database,
-				Logger:             logger,
-				MaxOpenConnections: 10,
-				MaxIdleConnections: 5,
-			}
-
-			if err := connection.Connect(); err != nil {
-				logger.Errorf("Failed to connect to Postgres [%s]: %v", dataSource.ConfigName, err)
-			}
-
-			externalDataSources[dataSource.ConfigName] = DataSource{
-				DatabaseType:   dataSource.Type,
-				DatabaseConfig: connection,
-				Initialized:    false,
-			}
-
+			ds := initPostgresDataSource(dataSource, logger)
+			externalDataSources[dataSource.ConfigName] = ds
 		default:
 			logger.Errorf("Unsupported database type '%s' for data source '%s'.", dataSource.Type, dataSource.Name)
 			continue
 		}
-
 		logger.Infof("Configured external data source: %s with config name: %s", dataSource.Name, dataSource.ConfigName)
 	}
-
 	return externalDataSources
+}
+
+func initMongoDataSource(dataSource DataSourceConfig, logger log.Logger) DataSource {
+	mongoURI := fmt.Sprintf("%s://%s:%s@%s:%s/%s",
+		dataSource.Type, dataSource.User, dataSource.Password, dataSource.Host, dataSource.Port, dataSource.Database)
+	if dataSource.Options != "" {
+		mongoURI += "?" + dataSource.Options
+	}
+	var params []string
+	if dataSource.SSL == "true" {
+		params = append(params, "ssl=true")
+	}
+	if dataSource.SSLCA != "" {
+		params = append(params, "tlsCAFile="+url.QueryEscape(dataSource.SSLCA))
+	}
+	if len(params) > 0 {
+		if strings.Contains(mongoURI, "?") {
+			mongoURI += "&" + strings.Join(params, "&")
+		} else {
+			mongoURI += "?" + strings.Join(params, "&")
+		}
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		logger.Errorf("Failed to connect to MongoDB [%s]: %v", dataSource.ConfigName, err)
+	} else if err := client.Ping(ctx, nil); err != nil {
+		logger.Errorf("Failed to ping MongoDB [%s]: %v", dataSource.ConfigName, err)
+	} else {
+		logger.Infof("Successfully connected to MongoDB [%s]", dataSource.ConfigName)
+	}
+	_ = client.Disconnect(ctx)
+	return DataSource{
+		DatabaseType: MongoDBType,
+		MongoURI:     mongoURI,
+		MongoDBName:  dataSource.Database,
+		Initialized:  false,
+	}
+}
+
+func initPostgresDataSource(dataSource DataSourceConfig, logger log.Logger) DataSource {
+	connectionString := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=%s",
+		dataSource.Type, dataSource.User, url.QueryEscape(dataSource.Password), dataSource.Host, dataSource.Port, dataSource.Database, dataSource.SSLMode)
+	if dataSource.SSLMode != "" {
+		connectionString += fmt.Sprintf("&sslrootcert=%s", url.QueryEscape(dataSource.SSLRootCert))
+		logger.Infof("Connection string: '%s' ", connectionString)
+	}
+	connection := &pg.Connection{
+		ConnectionString:   connectionString,
+		DBName:             dataSource.Database,
+		Logger:             logger,
+		MaxOpenConnections: 10,
+		MaxIdleConnections: 5,
+	}
+	if err := connection.Connect(); err != nil {
+		logger.Errorf("Failed to connect to Postgres [%s]: %v", dataSource.ConfigName, err)
+	}
+	return DataSource{
+		DatabaseType:   dataSource.Type,
+		DatabaseConfig: connection,
+		Initialized:    false,
+	}
 }
 
 // getDataSourceConfigs retrieves data source configurations from environment variables in the DATASOURCE_[NAME]_* format.
