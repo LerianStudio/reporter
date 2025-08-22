ee7493cf3f56ab2c80f8c9f0454eb0fc61f39d98
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-08-22 16:47:02 -0300

    feat: Removed unused code

--- components/frontend/src/core/infrastructure/container-registry/midaz/midaz-module.ts
+++
@@ -1,6 +0,0 @@
-import { Container, ContainerModule } from '../../utils/di/container'
-import { MidazHttpService } from '../../midaz/services/midaz-http-service'
-
-export const MidazModule = new ContainerModule((container: Container) => {
-  container.bind<MidazHttpService>(MidazHttpService).toSelf()
-})
